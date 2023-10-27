aeba6d6d6c5ce3ea16254dc677051b288c929851
Rendy Arya Kemal <renrror@gmail.com>
2023-10-27 20:06:39 +0700

    test: add string tests

--- test/string.test.ts
+++ test/string.test.ts
@@ -1,3 +1,76 @@
+import v from '../src/index';
+
 describe('string module', () => {
-  fail('todo');
+  test('normal', () => {
+    const validator = v.string();
+    expect(validator.parse('string')).toEqual({
+      success: true,
+      data: 'string',
+    });
+
+    expect(validator.parse('')).toEqual({
+      success: true,
+      data: '',
+    });
+
+    // @ts-expect-error
+    expect(validator.parse(1)).toEqual({
+      success: false,
+    });
+
+    // @ts-expect-error
+    expect(validator.parse([])).toEqual({
+      success: false,
+    });
+  });
+
+  test('with minimum length', () => {
+    const validator = v.string().minLength(5);
+    expect(validator.parse('string')).toEqual({
+      success: true,
+      data: 'string',
+    });
+
+    expect(validator.parse('five5')).toEqual({
+      success: true,
+      data: 'five5',
+    });
+
+    expect(validator.parse('')).toEqual({
+      success: false,
+    });
+  });
+
+  test('with maximum length', () => {
+    const validator = v.string().maxLength(5);
+    expect(validator.parse('five5')).toEqual({
+      success: true,
+      data: 'five5',
+    });
+
+    expect(validator.parse('')).toEqual({
+      success: true,
+      data: '',
+    });
+
+    expect(validator.parse('string')).toEqual({
+      success: false,
+    });
+  });
+
+  test('with min-max length', () => {
+    const validator = v.string().minLength(1).maxLength(5);
+    expect(validator.parse('five5')).toEqual({
+      success: true,
+      data: 'five5',
+    });
+
+    expect(validator.parse('')).toEqual({
+      success: false,
+    });
+
+    expect(validator.parse('string')).toEqual({
+      success: false,
+    });
+  });
 });
