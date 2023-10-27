564196cf026ba52bba51e7f88e345b1046e2cf62
Rendy Arya Kemal <renrror@gmail.com>
2023-10-27 19:56:18 +0700

    test: add enum tests

--- test/enum.test.ts
+++ test/enum.test.ts
@@ -1,3 +1,69 @@
+import v from '../src/index';
+
+enum Test {
+  ONE,
+  TWO,
+}
+
+enum TestValue {
+  ONE = 'one',
+  TWO = 'two',
+}
+
+enum Mixed {
+  ONE,
+  TWO = 9,
+}
+
 describe('enum module', () => {
-  fail('todo');
+  test('validate regular enum', () => {
+    const validator = v.enum(Test);
+    expect(validator.parse(Test.ONE.toString())).toEqual({
+      success: true,
+      data: Test.ONE,
+    });
+
+    expect(validator.parse('ONE')).toEqual({
+      success: true,
+      data: 'ONE',
+    });
+
+    expect(validator.parse('three')).toEqual({
+      success: false,
+    });
+  });
+
+  test('validate value enum', () => {
+    const validator = v.enum(TestValue);
+    expect(validator.parse(TestValue.ONE.toString())).toEqual({
+      success: true,
+      data: TestValue.ONE,
+    });
+
+    expect(validator.parse('one')).toEqual({
+      success: true,
+      data: 'one',
+    });
+
+    expect(validator.parse('three')).toEqual({
+      success: false,
+    });
+  });
+
+  test('validate mixed enum', () => {
+    const validator = v.enum(Mixed);
+    expect(validator.parse(Mixed.ONE.toString())).toEqual({
+      success: true,
+      data: Mixed.ONE,
+    });
+
+    expect(validator.parse('ONE')).toEqual({
+      success: true,
+      data: 'ONE',
+    });
+
+    expect(validator.parse('three')).toEqual({
+      success: false,
+    });
+  });
 });
